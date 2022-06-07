dbc0a5488e79301e61bae113e2908d58ecc69080
Saitore973 <saitorevg@gmail.com>
2022-06-08 00:23:35 +0300

    create heropage component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Portfolio from './Portfolio.js';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Heropage from './Heropage.js';
 
 function App() {
   return (
     <div className="App">
     <Portfolio /> 
+    <Heropage /> 
     </div>
   );
 }
---
+++ src/Heropage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Heropage(){
+    return(
+        <div className="heropage">
+        yooohhhh
+        </div>
+    )
+}
